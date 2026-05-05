8e94654dd4683f02d8dec4fdd9d5b99dfda77858
rhmn <rahmansrg77@gmail.com>
2026-05-05 10:23:04 +0700

    Demo 2 Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen, 
-      ),
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
       home: Home(),
     );
   }
 }
+
+class TodoMenuItem {
+  final String title;
+  final Icon icon;
+
+  TodoMenuItem({required this.title, required this.icon});
+}
+
+List<TodoMenuItem> foodMenuList = [
+  TodoMenuItem(
+    title: 'Fast Food',
+    icon: Icon(Icons.fastfood),
+  ),
+  TodoMenuItem(
+    title: 'Flight',
+    icon: Icon(Icons.flight),
+  ),
+];
 
 class Home extends StatefulWidget {
   @override
@@ -71,7 +86,6 @@ class _HomeState extends State<Home> {
             child: Column(
               children: <Widget>[
 
-                // Container, RichText
                 Container(
                   height: 100,
                   width: 100,
@@ -79,10 +93,7 @@ class _HomeState extends State<Home> {
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.orange,
                     boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 10,
-                      )
+                      BoxShadow(color: Colors.grey, blurRadius: 10)
                     ],
                   ),
                   child: Center(
@@ -101,12 +112,14 @@ class _HomeState extends State<Home> {
                               color: Colors.deepOrange,
                               fontWeight: FontWeight.bold,
                             ),
-                          ),
+                          )
                         ],
                       ),
                     ),
                   ),
                 ),
+
+                SizedBox(height: 20),
 
                 // Column, Row
                 Column(
@@ -124,9 +137,135 @@ class _HomeState extends State<Home> {
                   ],
                 ),
 
+                SizedBox(height: 20),
+
+                // Flat Buttons versi terbaru Flutter jadi Text Button
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Flag'),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Icon(Icons.flag),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.lightGreen,
+                        foregroundColor: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // RaisedButton versi terbaru Flutter jadi ElevatedButton
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Save'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Icon(Icons.save),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.lightGreen,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // IconButton
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.flight),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.flight),
+                      iconSize: 42,
+                      color: Colors.teal,
+                      tooltip: 'Flight',
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 20),
+
+                // PopupMenuButton
+                PopupMenuButton<TodoMenuItem>(
+                  icon: Icon(Icons.view_list),
+                  onSelected: (val) {
+                    print(val.title);
+                  },
+                  itemBuilder: (context) {
+                    return foodMenuList.map((item) {
+                      return PopupMenuItem<TodoMenuItem>(
+                        value: item,
+                        child: Row(
+                          children: [
+                            Icon(item.icon.icon),
+                            Padding(
+                              padding: EdgeInsets.all(8),
+                            ),
+                            Text(item.title),
+                          ],
+                        ),
+                      );
+                    }).toList();
+                  },
+                ),
+
+                SizedBox(height: 20),
+
+                // Button Bar
+                Container(
+                  color: Colors.white70,
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.map),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.airport_shuttle),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.brush),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+
               ],
             ),
           ),
+        ),
+      ),
+
+      // Floating Action Button
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.play_arrow),
+      ),
+
+      // Bottom Bar
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Colors.lightGreen.shade100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(Icons.pause),
+            Icon(Icons.stop),
+            Icon(Icons.access_time),
+            Padding(padding: EdgeInsets.all(32.0)),
+          ],
         ),
       ),
     );
